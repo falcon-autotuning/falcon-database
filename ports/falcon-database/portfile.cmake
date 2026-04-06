4bf20f514f5c1d0933bcb70cbe1e51e4682e5506
tjk144 <tkovach@wisc.edu>
2026-04-06 14:11:27 -0500

    The REF is not v1.0.0

--- ports/falcon-database/portfile.cmake
+++ ports/falcon-database/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO falcon-autotuning/falcon-database
-    REF 1.0.0
+    REF v1.0.0
     SHA512 ddca452d44a3fe8ca425d0536d5257e180d8ff0953399e47e5e361f926bb6129ccfc8e458a1666e43ba9d31fe47a952f3ce49eec656a7d8cfe34fa811f19cd95
 )
 vcpkg_cmake_configure(
